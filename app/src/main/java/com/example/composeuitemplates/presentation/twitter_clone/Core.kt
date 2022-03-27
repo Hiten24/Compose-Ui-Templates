388b8b1cea81072fd26443970e351a5f2803b679
Horizon733 <dishantgandhi733@gmail.com>
2022-03-27 23:23:39 +0530

    drawer and app bar completed

--- app/src/main/java/com/example/composeuitemplates/presentation/twitter_clone/Core.kt
+++ app/src/main/java/com/example/composeuitemplates/presentation/twitter_clone/Core.kt
@@ -1,18 +1,18 @@
 package com.example.composeuitemplates.presentation.twitter_clone
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Person
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.composeuitemplates.ui.theme.twitterBlack
 
 @Composable
 fun TextIcon(
@@ -20,15 +20,20 @@ fun TextIcon(
     text: String,
     contentDescription: String=""
 ){
-    Row{
+    Row(
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically
+    ){
         Icon(
             imageVector = icon,
-            contentDescription = contentDescription
+            contentDescription = contentDescription,
+            modifier = Modifier.size(24.dp)
         )
         Spacer(modifier = Modifier.width(8.dp))
         Text(
             text=text,
-            style = MaterialTheme.typography.body1
+            style = MaterialTheme.typography.body1,
+            fontSize = 20.sp
         )
     }
 }
--- app/src/main/java/com/example/composeuitemplates/presentation/twitter_clone/TwitterClone.kt
+++ app/src/main/java/com/example/composeuitemplates/presentation/twitter_clone/TwitterClone.kt
@@ -1,10 +1,15 @@
 package com.example.composeuitemplates.presentation.twitter_clone
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AutoAwesome
 import androidx.compose.material.icons.outlined.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
@@ -14,14 +19,44 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.composeuitemplates.R
-import com.example.composeuitemplates.ui.theme.ComposeUiTempletesTheme
-import com.example.composeuitemplates.ui.theme.twitterBlack
-import com.example.composeuitemplates.ui.theme.twitterxxLightGray
+import com.example.composeuitemplates.ui.theme.*
 import kotlinx.coroutines.launch
+
+
+val menuList = listOf(
+    SingleMenuItem(
+        Icons.Outlined.Person,
+        "Profile"
+    ),
+    SingleMenuItem(
+        Icons.Outlined.Description,
+        "Lists"
+    ),
+    SingleMenuItem(
+        Icons.Outlined.Topic,
+        "Topic"
+    ),
+    SingleMenuItem(
+        Icons.Outlined.BookmarkBorder,
+        "Bookmark"
+    ),
+    SingleMenuItem(
+        Icons.Outlined.Bolt,
+        "Moments"
+    ),
+    SingleMenuItem(
+        Icons.Outlined.Payments,
+        "Monetization"
+    )
+
+)
+
 
 @Composable
 fun TwitterClone(navController: NavController?=null){
@@ -32,20 +67,17 @@ fun TwitterClone(navController: NavController?=null){
         topBar = {
             TopAppBar(
                 title = {
-                    /*Row(
-                        horizontalArrangement = Arrangement.SpaceAround
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
                     ) {
-                        Image(
-                            painter = painterResource(R.drawable.gojo),
-                            contentDescription = null,
-                            modifier = Modifier.clip(CircleShape).size(42.dp)
-                        )
                         Icon(
                             painter = painterResource(R.drawable.ic_twitter),
                             contentDescription = null,
-                            modifier = Modifier.size(42.dp)
+                            modifier = Modifier.size(42.dp),
+                            tint = twitterBlue
                         )
-                    }*/
+                    }
 
                 },
                 navigationIcon = {
@@ -62,7 +94,17 @@ fun TwitterClone(navController: NavController?=null){
                     }
                 },
                 backgroundColor = Color.White,
-                contentColor = twitterBlack
+                contentColor = twitterBlack,
+                modifier = Modifier.fillMaxWidth().padding(6.dp),
+                actions = {
+
+                    Icon(
+                        imageVector = Icons.Outlined.AutoAwesome,
+                        contentDescription = null,
+                        modifier = Modifier.size(30.dp)
+                    )
+                },
+                elevation = 0.dp
             )
         },
         drawerContent = {
@@ -82,90 +124,135 @@ fun drawerContent(){
     Column(
         horizontalAlignment = Alignment.Start
     ){
-        Column {
+        Text(
+            text = "Account Center",
+            style = TextStyle(
+                color = twitterBlack,
+                fontWeight = FontWeight.Bold,
+                fontSize = 18.sp
+            ),
+            modifier = Modifier.padding(8.dp)
+        )
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+//            Spacer(modifier = Modifier.height(8.dp))
             Image(
                 painter = painterResource(R.drawable.gojo),
                 contentDescription = null,
-                modifier = Modifier.clip(CircleShape).size(50.dp)
+                modifier = Modifier.clip(CircleShape).size(40.dp)
             )
             Text(
                 text = "Gojo Satoru",
-                style = MaterialTheme.typography.body1
+                style = TextStyle(
+                    color = twitterBlack,
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 18.sp
+                )
             )
             Text(
                 text = "@gojo",
-                style = MaterialTheme.typography.body2
+                style = TextStyle(
+                    color = twitterDarkGray,
+                    fontWeight = FontWeight.Light,
+                    fontSize = 16.sp
+                )
             )
-        }
-        Divider()
-        Spacer(modifier = Modifier.height(8.dp))
-        Column(
-            modifier = Modifier.padding(16.dp)
-        ) {
-            TextIcon(
-                icon = Icons.Outlined.Person,
-                contentDescription = "",
-                text = "Profile"
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            TextIcon(
-                icon = Icons.Outlined.Description,
-                contentDescription = "",
-                text = "Lists"
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            TextIcon(
-                icon = Icons.Outlined.Topic,
-                contentDescription = "",
-                text = "Topic"
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            TextIcon(
-                icon = Icons.Outlined.BookmarkBorder,
-                contentDescription = "",
-                text = "Bookmark"
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            TextIcon(
-                icon = Icons.Outlined.Bolt,
-                contentDescription = "",
-                text = "Moments"
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            TextIcon(
-                icon = Icons.Outlined.Payments,
-                contentDescription = "",
-                text = "Monetization"
-            )
-        }
-        Divider()
-        Spacer(modifier = Modifier.height(8.dp))
-        Column {
-            Text(
-                text = "Settings and Privacy"
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(
-                text = "Help Center"
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-        }
-        Divider()
-        Spacer(modifier = Modifier.height(8.dp))
-        Column {
             Row(
-                horizontalArrangement = Arrangement.SpaceAround
-            ) {
-                Icon(
-                    imageVector = Icons.Outlined.Lightbulb,
-                    contentDescription = null
+                modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp),
+                horizontalArrangement = Arrangement.Start
+            ){
+                Text(
+                    text="5",
+                    style = TextStyle(
+                        color = twitterBlack,
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 16.sp
+                    )
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(
+                    text = "Following",
+                    style = TextStyle(
+                        color = twitterDarkGray,
+                        fontWeight = FontWeight.Light,
+                        fontSize = 16.sp
+                    )
                 )
                 Spacer(modifier = Modifier.width(8.dp))
-                Icon(
-                    imageVector = Icons.Outlined.QrCode,
-                    contentDescription = "",
+                Text(
+                    text = "1M",
+                    style = TextStyle(
+                        color = twitterBlack,
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 16.sp
+                    )
                 )
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(
+                    text = "Followers",
+                    style = TextStyle(
+                        color = twitterDarkGray,
+                        fontWeight = FontWeight.Light,
+                        fontSize = 16.sp
+                    )
+                )
+
             }
+        }
+        Divider()
+        Spacer(modifier = Modifier.height(16.dp))
+        LazyColumn(
+            modifier = Modifier.padding(horizontal = 16.dp),
+            horizontalAlignment = Alignment.Start
+        ) {
+            items(menuList){ menuItem ->
+                Spacer(modifier = Modifier.height(8.dp))
+                TextIcon(
+                    icon = menuItem.icon,
+                    contentDescription = menuItem.text,
+                    text = menuItem.text
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+        }
+        Divider()
+        Spacer(modifier = Modifier.height(24.dp))
+        Column(
+            modifier = Modifier.padding(horizontal = 16.dp)
+        ) {
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = "Settings and Privacy",
+                style = MaterialTheme.typography.body1,
+                fontSize = 20.sp
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = "Help Center",
+                style = MaterialTheme.typography.body1,
+                fontSize = 20.sp
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+        }
+        Divider()
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth().padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Icon(
+                imageVector = Icons.Outlined.Lightbulb,
+                contentDescription = null,
+                modifier = Modifier.size(40.dp)
+            )
+            Icon(
+                imageVector = Icons.Outlined.QrCode,
+                contentDescription = "",
+                modifier = Modifier.size(40.dp)
+            )
         }
     }
 }
